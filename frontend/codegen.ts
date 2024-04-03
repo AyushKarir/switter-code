a42e4c6ec6ba8e02cad52913a6ceba6a73c41e11
ayushkarir <ayush.karir@gmail.com>
2024-04-03 21:07:29 +0530

    env vars

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://switter.vercel.app/graphql",
+  schema: "https://switterspace.vercel.app/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
